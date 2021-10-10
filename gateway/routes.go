fe80b3e1a6adffd62adfd1d4c3547d477118e1f2
Matt Knop <knopmatthew93@gmail.com>
2021-10-09 20:51:38 -0600

    Created separate package for gateway connection.

---
+++ gateway/routes.go
@@ -0,0 +1,49 @@
+package gateway
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	log "github.com/sirupsen/logrus"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info("Gateway connection established!")
+
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		// read in a message
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		// print out that message for clarity
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+	}
+}
