ff548db2db273d48321d5cd56c2ec3dd06a1c177
Matthew Knop <knopmatthew93@gmail.com>
2022-05-01 19:45:07 -0600

    Fixed logging messages.

--- src/gateway/connection.go
+++ src/gateway/connection.go
@@ -1,6 +1,8 @@
 package gateway
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/signal"
@@ -17,22 +19,26 @@ var (
 	interrupt chan os.Signal
 )
 
-func EstablishConnection() {
+func EstablishConnection(ctx context.Context) {
 	interrupt = make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
 
 	signal.Notify(interrupt, os.Interrupt) // Notify the interrupt channel for SIGINT
 
-	connection, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	connection, _, err := websocket.DefaultDialer.DialContext(ctx, socketUrl, nil)
 	if err != nil {
-		log.Printf("%s: Connection could not be established: %s", curr_time, err)
+		log.Fatalf("%s: Connection could not be established: %s", curr_time, err)
 	}
 
+	connection.EnableWriteCompression(true)
+	connection.SetCompressionLevel(1)
+
 	for {
+		log.Printf("%s: Waiting for response from server.", curr_time)
 		heartbeat_interval, err := ReceiveMessage(connection)
 		if err != nil {
-			panic(fmt.Sprintf("%s: Could not retrieve heartbeat interval: %s", curr_time, err))
+			log.Fatalf("%s: Could not retrieve heartbeat interval: %s", curr_time, err)
 		} else {
-			log.Printf("%s: Value of heartbeat interval is: %d milliseconds (%d seconds)", curr_time, heartbeat_interval, heartbeat_interval/1000)
+			log.Printf("%s: SUCCESS: Value of heartbeat interval is: %d milliseconds (%d seconds)", curr_time, heartbeat_interval, heartbeat_interval/1000)
 		}
 
 		time.Sleep(time.Duration(heartbeat_interval))
@@ -44,19 +50,21 @@ func EstablishConnection() {
 func ReceiveMessage(connection *websocket.Conn) (int, error) {
 	_, msg, err := connection.ReadMessage()
 	if err != nil {
-		panic(fmt.Sprintf("%s: Error receiving message: %s", curr_time, err))
+		log.Fatalf("%s: Error receiving message: %s", curr_time, err)
+	} else {
+		log.Printf("%s: Received: %s\n", curr_time, msg)
 	}
 
-	log.Printf("%s: Received: %s\n", curr_time, msg)
-
+	msgData := []string{}
+	jsonMsg, _ := json.MarshalIndent(&msgData, "", " ")
+	log.Printf("json data: %s", jsonMsg)
 	return strconv.Atoi(string(msg[53:58]))
 }
 
 func SendMessage(connection *websocket.Conn) {
 	err := connection.WriteMessage(websocket.TextMessage, []byte("op: 1, d: 251"))
 	if err != nil {
-		log.Println(fmt.Sprintf("%s: Error during writing to websocket: %s", curr_time, err))
-		panic("Shutting down...")
+		log.Fatalf(fmt.Sprintf("%s: Error during writing to websocket: %s", curr_time, err))
 	} else {
 		log.Printf("%s: Message sent back to server.", curr_time)
 	}
--- src/main.go
+++ src/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/disc-go/src/gateway"
 )
 
 func main() {
+	ctx := context.Background()
 	fmt.Println("disc-go!")
 
-	gateway.EstablishConnection()
+	gateway.EstablishConnection(ctx)
 }
