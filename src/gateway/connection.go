ffc9b863b1853b83a1b968ce24f28aa722554ec2
Matthew Knop <knopmatthew93@gmail.com>
2022-05-12 23:21:08 -0600

    Added function to receive ack from server (Still in progress).

--- src/gateway/connection.go
+++ src/gateway/connection.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
-	"os/signal"
 	"strings"
 	"time"
 
@@ -17,14 +15,9 @@ import (
 var (
 	socketUrl = "wss://gateway.discord.gg/?v=9&encoding=json"
 	curr_time = time.Now().Format(time.Kitchen)
-	interrupt chan os.Signal
 )
 
 func EstablishConnection(ctx context.Context) {
-	interrupt = make(chan os.Signal) // Channel to listen for interrupt signal to terminate gracefully
-
-	signal.Notify(interrupt, os.Interrupt) // Notify the interrupt channel for SIGINT
-
 	connection, _, err := websocket.DefaultDialer.DialContext(ctx, socketUrl, nil)
 	if err != nil {
 		log.Fatalf("%s: Connection could not be established: %s", curr_time, err)
@@ -33,31 +26,32 @@ func EstablishConnection(ctx context.Context) {
 	connection.EnableWriteCompression(true)
 	connection.SetCompressionLevel(1)
 
-	for {
-		//jitter := rand.Float32()
-		log.Printf("%s: Waiting for response from server.", curr_time)
-		heartbeat_interval, sequence_num := ReceiveMessage(connection)
-		if err != nil {
-			log.Fatalf("%s: Could not retrieve heartbeat interval: %s", curr_time, err)
-		} else {
-			log.Printf("%s: SUCCESS: Value of heartbeat interval is: %d milliseconds", curr_time, heartbeat_interval)
-		}
-
-		//time.Sleep(time.Duration(float32(heartbeat_interval) * jitter))
-		time.Sleep(time.Duration(*heartbeat_interval))
-		SendMessage(connection, sequence_num)
+	log.Printf("%s: [ OP CODE 10 ] Sending initial request to Discord Gateway server", curr_time)
+	heartbeat_interval, sequence_num := ReceiveMessage(connection)
+	if err != nil {
+		log.Fatalf("%s: [ OP CODE 10 ] Could not retrieve heartbeat interval: %s", curr_time, err)
+	} else {
+		log.Printf("%s: [ OP CODE 10 ] Value of heartbeat interval is: %d seconds", curr_time, (heartbeat_interval / 1000))
 	}
+
+	time.Sleep(time.Duration((heartbeat_interval / 1000) * int(time.Second)))
+
+	SendMessage(connection, sequence_num)
+
+	time.Sleep(time.Duration((heartbeat_interval / 1000) * int(time.Second)))
+
+	ACK(connection)
 }
 
-func ReceiveMessage(connection *websocket.Conn) (*int, *int) {
+func ReceiveMessage(connection *websocket.Conn) (int, *int) {
 	_, msg, err := connection.ReadMessage()
 	if err != nil {
-		log.Fatalf("%s: Error receiving message: %s", curr_time, err)
+		log.Fatalf("%s: [ OP CODE 10 ] Error receiving message: %s", curr_time, err)
 	} else {
-		log.Printf("%s: Received: %s\n", curr_time, msg)
+		log.Printf("%s: [ OP CODE 10 ] Received: %s\n", curr_time, msg)
 	}
 
-	var op_10_hello Payload
+	var op_10_hello OP_10
 
 	if err := json.NewDecoder(bytes.NewReader(msg)).Decode(&op_10_hello); err != nil {
 		log.Fatalf("%s: Error parsing json data: %s", curr_time, err)
@@ -67,17 +61,12 @@ func ReceiveMessage(connection *websocket.Conn) (*int, *int) {
 }
 
 func SendMessage(connection *websocket.Conn, sequence_num *int) {
-	var op_code *int
-	op_code = new(int)
-	*op_code = 1
-
-	op_1_heartbeat := Payload{
-		OP: op_code,
-		D:  Data{Sequence: sequence_num},
+	op_1_heartbeat := OP_1{
+		OP: 1,
+		D:  sequence_num,
 	}
 
 	op_1_heartbeat_json, err := json.Marshal(op_1_heartbeat)
-	println(strings.ToLower(string(op_1_heartbeat_json)))
 	if err != nil {
 		log.Fatalf("%s: Error converting to json data: %s", curr_time, err)
 	}
@@ -86,6 +75,22 @@ func SendMessage(connection *websocket.Conn, sequence_num *int) {
 	if err != nil {
 		log.Fatalf(fmt.Sprintf("%s: Error during writing to websocket: %s", curr_time, err))
 	} else {
-		log.Printf("%s: Message sent back to server.", curr_time)
+		log.Printf("%s: [ OP CODE 01 ] Sending the following payload: %s", curr_time, strings.ToLower(string(op_1_heartbeat_json)))
 	}
+}
+
+func ACK(connection *websocket.Conn) {
+	_, msg, err := connection.ReadMessage()
+	if err != nil {
+		log.Fatalf("%s: [ OP CODE 11 ] Error receiving message: %s", curr_time, err)
+	} else {
+		log.Printf("%s: [ OP CODE 11 ] Received: %s\n", curr_time, msg)
+	}
+
+	var op_11_ack OP_11
+
+	if err := json.NewDecoder(bytes.NewReader(msg)).Decode(&op_11_ack); err != nil {
+		log.Fatalf("%s: Error parsing json data: %s", curr_time, err)
+	}
+
 }
