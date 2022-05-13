36eaa896b12aa0038655cddf865ea45e1178a609
Matthew Knop <knopmatthew93@gmail.com>
2022-05-12 23:19:55 -0600

    Type definitions for op codes 10, 1, and 11.

--- src/gateway/opcodes.go
+++ src/gateway/opcodes.go
@@ -4,7 +4,7 @@ package gateway
 opcode 10 hello
 - Server reponse after client sends initial request to the server
 */
-type Payload struct {
+type OP_10 struct {
 	OP *int    `json:"op"`
 	D  Data    `json:"d"`
 	S  *int    `json:"s"`
@@ -12,6 +12,15 @@ type Payload struct {
 }
 
 type Data struct {
-	Heartbeat_Interval *int `json:"heartbeat_interval"`
+	Heartbeat_Interval int  `json:"heartbeat_interval"`
 	Sequence           *int `json:"seq"`
+}
+
+type OP_1 struct {
+	OP int  `json:"op"`
+	D  *int `json:"d"`
+}
+
+type OP_11 struct {
+	OP int `json:"op"`
 }
