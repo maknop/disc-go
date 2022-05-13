3be089451aad8bcb65e46a047580111c45eb316a
Matthew Knop <knopmatthew93@gmail.com>
2022-05-12 23:20:31 -0600

    Added goroutine to wait for user exiting the program.

--- src/main.go
+++ src/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/maknop/disc-go/src/gateway"
 )
+
+func init() {
+	TerminateGracefully()
+}
 
 func main() {
 	ctx := context.Background()
 
 	gateway.EstablishConnection(ctx)
+}
+
+func TerminateGracefully() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		fmt.Println("EXITING: Ctrl+C pressed in Terminal")
+		os.Exit(0)
+	}()
 }
