2cdfe66ce005207e32d6edf580871b4a5b1df50a
maknop <knopmatthew@gmail.com>
2022-05-21 16:05:57 -0600

    Moved opcode definitions to separate package. Working on opcode 2.

--- src/gateway/connection.go
+++ src/gateway/connection.go
@@ -5,15 +5,16 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
 	"time"
+
+	opcodes "github.com/maknop/disc-go/src/types"
 
 	"github.com/gorilla/websocket"
 	log "github.com/sirupsen/logrus"
 )
 
 var (
-	socketUrl = "wss://gateway.discord.gg/?v=9&encoding=json"
+	socketUrl = "wss://gateway.discord.gg/?v=9&encoding=json&compress?=true"
 	curr_time = time.Now().Format(time.Kitchen)
 )
 
@@ -41,6 +42,8 @@ func EstablishConnection(ctx context.Context) {
 	time.Sleep(time.Duration((heartbeat_interval / 1000) * int(time.Second)))
 
 	ACK(connection)
+
+	Identity(connection)
 }
 
 func ReceiveMessage(connection *websocket.Conn) (int, *int) {
@@ -51,7 +54,7 @@ func ReceiveMessage(connection *websocket.Conn) (int, *int) {
 		log.Printf("%s: [ OP CODE 10 ] Received: %s\n", curr_time, msg)
 	}
 
-	var op_10_hello OP_10_Hello
+	var op_10_hello opcodes.OP_10_Hello
 
 	if err := json.NewDecoder(bytes.NewReader(msg)).Decode(&op_10_hello); err != nil {
 		log.Fatalf("%s: Error parsing json data: %s", curr_time, err)
@@ -61,21 +64,16 @@ func ReceiveMessage(connection *websocket.Conn) (int, *int) {
 }
 
 func SendMessage(connection *websocket.Conn, sequence_num *int) {
-	op_1_heartbeat := OP_1_Heartbeat{
+	op_1_heartbeat := opcodes.OP_1_Heartbeat{
 		OP: 1,
-		D:  sequence_num,
+		D:  opcodes.OP_1_Heartbeat_Data{Sequence: sequence_num},
 	}
 
-	op_1_heartbeat_json, err := json.Marshal(op_1_heartbeat)
-	if err != nil {
-		log.Fatalf("%s: Error converting to json data: %s", curr_time, err)
-	}
-
-	err = connection.WriteJSON(op_1_heartbeat_json)
+	err := connection.WriteJSON(op_1_heartbeat)
 	if err != nil {
 		log.Fatalf(fmt.Sprintf("%s: Error during writing to websocket: %s", curr_time, err))
 	} else {
-		log.Printf("%s: [ OP CODE 01 ] Sending the following payload: %s", curr_time, strings.ToLower(string(op_1_heartbeat_json)))
+		log.Printf("%s: [ OP CODE 01 ] Sending the following payload: {op: %d, d: {Seq: %v}}", curr_time, op_1_heartbeat.OP, op_1_heartbeat.D.Sequence)
 	}
 }
 
@@ -87,10 +85,32 @@ func ACK(connection *websocket.Conn) {
 		log.Printf("%s: [ OP CODE 11 ] Received: %s\n", curr_time, msg)
 	}
 
-	var op_11_ack OP_11_Heartbeat_ACK
+	var op_11_ack opcodes.OP_11_Heartbeat_ACK
 
 	if err := json.NewDecoder(bytes.NewReader(msg)).Decode(&op_11_ack); err != nil {
 		log.Fatalf("%s: Error parsing json data: %s", curr_time, err)
 	}
 
+}
+
+func Identity(connection *websocket.Conn) {
+	op_2_identity := opcodes.OP_2_Identity{
+		OP: 2,
+		D: opcodes.OP_2_Identity_Data{
+			Token:   "",
+			Intents: 7,
+			Properties: opcodes.OP_2_Identity_Properties{
+				OS:      "Linux",
+				Browser: "disc-go",
+				Device:  "disc-go",
+			},
+		},
+	}
+
+	err := connection.WriteJSON(op_2_identity)
+	if err != nil {
+		log.Fatalf(fmt.Sprintf("%s: Error during writing to websocket: %s", curr_time, err))
+	} else {
+		log.Printf("%s: [ OP CODE 02 ] Sending the following payload: ", curr_time)
+	}
 }
