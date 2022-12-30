dbb63bde93dc8586b7856eeccfa0e3ceab7e6be3
Matthew Knop <knopmatthew93@gmail.com>
2022-12-29 23:32:14 -0700

    Custom logging

--- client/client.go
+++ client/client.go
@@ -3,13 +3,27 @@ package client
 import (
 	"context"
 	"fmt"
+	"os"
 
 	gateway "github.com/maknop/disc-go/gateway"
 	utils "github.com/maknop/disc-go/utils"
+	"github.com/sirupsen/logrus"
+	formatter "github.com/x-cray/logrus-prefixed-formatter"
 )
 
 type User struct {
 	auth_token string
+}
+
+var logger = &logrus.Logger{
+	Out:   os.Stderr,
+	Level: logrus.DebugLevel,
+	Formatter: &formatter.TextFormatter{
+		DisableColors:   true,
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   true,
+		ForceFormatting: true,
+	},
 }
 
 func AuthenticateUser(authToken string) error {
